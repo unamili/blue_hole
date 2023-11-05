3bf739c39410dee49ab27b448bfdf04d3c3c5fa6
unamili <miliurricariet@gmail.com>
2023-11-05 20:13:02 -0300

    Separacion de transectas sinopticas, sin est repetidas

---
+++ proba_clase_correl.py
@@ -0,0 +1,7 @@
+
+data #definido en otro script. es un perfil CTD
+lat = data.data['latitude']
+lon = data.data['longitude']
+plt.scatter(data.data['prDM'], data.data['t090C'])
+plt.scatter(data.data['prDM'], data.data['sal11'])
+
