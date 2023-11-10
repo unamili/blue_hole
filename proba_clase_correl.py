2fcbb10505d2c8110400f0c343c77a751f6069f9
Milagro Urricariet <54715537+unamili@users.noreply.github.com>
2023-11-10 11:39:50 -0300

    Delete proba_clase_correl.py

--- proba_clase_correl.py
+++
@@ -1,7 +0,0 @@
-
-data #definido en otro script. es un perfil CTD
-lat = data.data['latitude']
-lon = data.data['longitude']
-plt.scatter(data.data['prDM'], data.data['t090C'])
-plt.scatter(data.data['prDM'], data.data['sal11'])
-
